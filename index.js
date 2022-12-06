b00b3edb128986ab3a188250d67eab5f616543c9
Caio Labella <88745840+Caiollaz@users.noreply.github.com>
2022-12-06 19:20:20 -0300

    Criado script
    foi criado funções para poder ser chamada na hora de Encrypt/Decrypt

---
+++ index.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+    console.log('JS Iniciado')
+        
+});
+
+function Encrypt(){
+    
+    let descricao = $("#descricao").val();
+    let crypt = cryptpru(descricao);
+    
+    $("#resultado").val(crypt);
+
+}
+
+function Decrypt(){
+
+    let descricao = $("#descricao").val();
+    let crypt = decryptpru(descricao);
+    
+    $("#resultado").val(crypt);
+    
+}
+
+const cryptpru = (phrase) => {
+    return phrase.split(' ').map((word) => {
+        const wordLetters = word.split('')
+        return wordLetters.map((letter, index) => {
+
+            const charCode = letter.charCodeAt(0);
+            const binary = charCode.toString(2).padStart(8, 0)
+            const prus = binary.replace(/0/g, 'pru, ').replace(/1/g, 'pru pru, ').replace(/, $/, '')
+ 
+            return prus
+
+        }).join('-')
+    }).join('~')
+}
+
+const decryptpru = (prurase) => {
+    return prurase.split('~') .map((word) => {
+        return word.split('-').map((letter) => {
+
+            const binary = letter.replace(/pru pru(, )*/g, '1').replace(/pru(, )*/g, '0')
+            return String.fromCharCode(parseInt(binary, 2))
+
+        }).join('')
+    }).join(' ')
+}
